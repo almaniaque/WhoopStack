f62f7b88f7d159a22051b48eb296c4c78cab86ce
davy Thiebaut <davy.du.60@hotmail.fr>
2026-06-23 17:27:46 +0200

    maj topbar

--- angular/my-app/src/app/app.ts
+++ angular/my-app/src/app/app.ts
@@ -1,14 +1,11 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TopBarComponent } from './topbar/topbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  template: `
-    <router-outlet></router-outlet>
-  `
-
+  imports: [RouterOutlet, TopBarComponent],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('my-app');
-}
+export class App { }
