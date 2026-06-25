523060297d0ee70ed2c3610663171bf9eaa76a72
Aurore Dufour <aurore.d2@hotmail.fr>
2026-06-25 09:05:25 +0200

    maj

--- angular/my-app/src/app/accueil/accueil.ts
+++ angular/my-app/src/app/accueil/accueil.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { StatCard } from './stat-card/stat-card';
+import { QuickAccessCard } from './quick-access-card/quick-access-card';
 
 @Component({
   selector: 'app-accueil',
-  imports: [],
+  imports: [StatCard, QuickAccessCard],
   templateUrl: './accueil.html',
   styleUrl: './accueil.css',
 })
