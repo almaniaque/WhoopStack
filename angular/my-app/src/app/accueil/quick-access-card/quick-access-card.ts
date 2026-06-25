54f55ada379ba900c7caf441d6dcd44b299a3511
Aurore Dufour <aurore.d2@hotmail.fr>
2026-06-25 09:06:11 +0200

    maj

---
+++ angular/my-app/src/app/accueil/quick-access-card/quick-access-card.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quick-access-card',
+  imports: [],
+  templateUrl: './quick-access-card.html',
+  styleUrl: './quick-access-card.css',
+})
+export class QuickAccessCard {}
